e73b30b3a88169427c4edd88865e27ea9a83c97b
Biruk Gebru <biruk.gebru28@gmail.com>
2026-06-22 19:15:14 +0300

    fix: resolve merge confilicts

--- src/main/java/com/workflowsystem/demo/file/repository/AttachmentRepository.java
+++ src/main/java/com/workflowsystem/demo/file/repository/AttachmentRepository.java
@@ -1,19 +1,8 @@
 package com.workflowsystem.demo.file.repository;
 
-<<<<<<< HEAD
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.workflowsystem.demo.file.entity.Attachment;
-
-public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
-    Optional<Attachment> findByWorkflowRequestId(Long workflowId);
-=======
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.workflowsystem.demo.file.entity.Attachment;
 public interface AttachmentRepository extends JpaRepository<Attachment, Long> {
     List<Attachment> findByWorkflowRequestId(Long workflowId);
->>>>>>> staging/main
 }
